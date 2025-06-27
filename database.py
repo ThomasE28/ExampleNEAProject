d0dd752b04cccffc4ab765749dc9ddc41c7b1ff3
ThomasE28 <7997@balcarras.gloucs.sch.uk>
2025-06-27 13:24:28 +0000

    Pending changes exported from your codespace

---
+++ database.py
@@ -0,0 +1,25 @@
+import sqlite3 as sql 
+
+class DatabaseHandler:
+    def __init__(self, dbName = "appData.db"):
+        self.dbName = dbName
+
+    def connect(self):
+        return sql.connect(self.dbName)
+    
+    def createTables(self):
+        with self.connect() as conn:
+            conn.execute("""CREATE TABLE IF NOT EXISTS users (
+                         userID INTEGER PRIMARY KEY AUTOINCREMENT,
+                         username TEXT UNIQUE NOT NULL,
+                         password TEXT NOT NULL
+                         )""")
+            
+    def createUser(self, username, password):
+        try:
+            with self.connect() as conn:
+                conn.execute("INSERT INTO users (username, password) VALUES (?, ?)", (username, password))
+                conn.commit()
+                return True
+        except: 
+            return False
